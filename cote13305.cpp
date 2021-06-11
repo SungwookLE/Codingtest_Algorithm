905f3e3864dc7e043355ca54b21446e5cd603080
SungwookLE <joker1251@naver.com>
2021-06-11 17:33:21 +0900

    feat: #13305 udpated, why it's not correct?

---
+++ cote13305.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+class solver_13305{
+    public:
+    solver_13305(std::vector<long long int> distance, std::vector<long long int> cost){
+        distances = distance;
+        costs = cost;
+        N = costs.size();
+    }
+
+    int solver(){
+        long long int total_cost = 0;
+        long long int now_cost = costs[0];
+        long long int next_cost;
+        for(int i =1 ; i < N ; ++i){
+            next_cost = costs[i];
+
+            if (now_cost > next_cost){
+                total_cost += distances[i-1] * now_cost;
+                now_cost = costs[i];
+            }
+            else{
+                total_cost += distances[i-1] * now_cost;
+            }
+        }
+        
+        return total_cost;
+    }
+
+    private:
+    std::vector<long long int> distances, costs;
+    int N;
+
+};
+
+int main(){
+
+    int N; // 도시의 개수
+    std::cin >> N;
+
+    long long int distance;
+    std::vector<long long int> distances;
+    for(int i = 0 ; i < (N-1) ; ++i){
+        std::cin >> distance;
+        distances.push_back(distance);
+    }
+
+    long long int cost;
+    std::vector<long long int> costs;
+    for(int i =0 ; i < N ; ++i){
+        std::cin >> cost;
+        costs.push_back(cost);
+    }
+
+    solver_13305 solver(distances, costs);
+    long long int answer = solver.solver();
+
+    std::cout << answer << std::endl;
+    return 0;
+}
+
+/*
+일단은, 지금 도시의 가격보다 다음 도시의 가겨이 싸면, 딱 이동할 거리만큼만 주유를 하고,
+지금 도시의 가격이 더 싸면 다음 도시 + 다음 도시까지 주유를 하고 다음+다음도시와 가격비교
+*/
