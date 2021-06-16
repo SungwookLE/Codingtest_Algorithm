f52de1ed425c1d2638555964893b7baaf967c15e
SungwookLE <joker1251@naver.com>
2021-06-16 21:00:49 +0900

    feat: #11051, #1010 using DP, pascal triangle method updated

--- cote11051.cpp
+++ cote11051.cpp
@@ -12,19 +12,45 @@
 */
 class solver_11051{
     public:
+    solver_11051(){
+        std::vector<int> v(1001,0);
+        DP.resize(1001, v);
+    }
+
     void insert_number(){
         std::cin >> N >> K;
     }
 
     void solver(){
 
+        // 파스칼의 삼각형
+        // REF: https://hevton.tistory.com/377
 
+        for(int i =1 ; i <= N; ++i){
+            for(int j =0 ; j <= i; ++j){
 
+                if ( j == 0 | i ==j )
+                    DP[i][j] = 1;
+                else
+                    DP[i][j] = (DP[i-1][j] + DP[i-1][j-1]) % 10007;
+
+            }
+        }
+
+        std::cout << DP[N][K] << std::endl;
     }
 
     private:
     int N, K;
-    std::vector<int> DP;
+    std::vector<std::vector<int>> DP;
     
 };
+
+int main(){
+    solver_11051 solver;
+    solver.insert_number();
+    solver.solver();
+
+
+}
 
