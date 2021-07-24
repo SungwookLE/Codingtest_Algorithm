1518e478371d02306bf11f6695f6e86dc80c414e
SungwookLE <joker1251@naver.com>
2021-07-24 22:04:58 +0900

    feat: #6549 ,, almost clear

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,31 +1,87 @@
-// 세그먼트 트리를 구해보자 O(logN)
-// https://blog.naver.com/ndb796/221282210534, 구간합 트리
 #include <iostream>
 #include <vector>
-// 1. 트리 만들기
+// https://blog.naver.com/ndb796/221282210534, 구간합 트리
+// 구간합 세그먼트 트리 O(logN)
 
-std::vector<int> a = {1, 9, 3, 8, 4, 5, 5, 9, 10, 3, 4 ,5};
-std::vector<int> tree(a.size()*4);
+class segment_tree{
+    public:
+        // 트리 클래스
+        segment_tree(std::vector<int> _arr){
+            given_arr = _arr;
+            tree.resize(4*given_arr.size());
+        }
+        int init(int start, int end, int node);
+        int sum(int start, int end, int node, int left, int right);
+        void show_tree(){
+            std::cout << "TREE: \n";
+            for(int i = 1 ; i < tree.size(); i=i*2+1){
+                for(int j =(i-1)/2+1; j <= i ; ++j)
+                    std::cout << tree[j] << " ";
+                std::cout << std::endl;
+            }
+        }
+        void show_given_arr(){
+            std::cout<<"Given arr: \n";
+            for(auto a : given_arr)
+                std::cout << a<< " ";
+            std::cout << std::endl;
+        }
+        void update(int start, int end, int node, int index, int dif);
+    private:
+    std::vector<int> tree ={-1};
+    std::vector<int> given_arr;
+};
 
+int main(){
+    int N;
+    std::cin>> N;
+    std::vector<int> given(N);
+    for(int i =0 ; i < N ; ++i)
+        std::cin >> given[i];
 
-// start: 시작 인덱스, end: 끝 인덱스
-int init(int start, int end, int node){
+    segment_tree st(given);
 
-    if (start == end)
-        return tree[node] = a[start];
     
-    int mid = (start+end)/2;
-    // 재귀적으로 두 부분을 나눈 뒤에 그 합을 자기 자신으로 합니다.
-    return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2+1);
+
+    st.init(0, given.size()-1, 1);
+    st.show_given_arr();
+    st.show_tree();
+
+    std::cout << "\nSUM: idx[1~2] \n";
+    std::cout << st.sum(0, given.size()-1, 1, 1, 2) << std::endl;
+
+    std::cout << "UPDATE: idx[1] to 10 \n";
+    st.update(0, given.size()-1, 1, 1, 10);
+    st.show_tree();
+    
+    std::cout << "\nSUM: idx[1~2] \n";
+    std::cout << st.sum(0, given.size()-1, 1, 1, 2) << std::endl;
+
+    return 0;
 }
 
 // start: 시작 인덱스, end: 끝 인덱스
+int segment_tree::init(int start, int end, int node){
+    if (start == end){
+        tree[node] = given_arr[start];
+        return tree[node];
+    }
+
+    int mid = (start+end)/2;
+    // 재귀적으로 두 부분을 나눈 뒤에 그 합을 자기 자신으로 합니다.
+    tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2+1);
+
+    return tree[node];
+}
+// start: 시작 인덱스, end: 끝 인덱스
 // left, right: 구간 합을 구하고자 하는 범위
-int sum(int start, int end, int node, int left, int right){
+int segment_tree::sum(int start, int end, int node, int left, int right){
     //범위 밖에 있는 경우
-    if (left > end || right < start) return 0;
+    if (left > end || right < start)
+        return 0;
     // 범위 안에 있는 경우
-    if (left <= start && end <= right) return tree[node];
+    if (left <= start && end <=right)
+        return tree[node];
     //그렇지 않다면 두 부분으로 나누어 합을 구하기
     int mid = (start+end) / 2;
     return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
@@ -34,27 +90,14 @@ int sum(int start, int end, int node, int left, int right){
 // start: 시작 인덱스, end: 끝 인덱스
 // index: 구간 합을 수정하고자 하는 노드
 // dif: 수정할 값
-void update(int start, int end, int node, int index, int dif){
-
+void segment_tree::update(int start, int end, int node, int index, int dif){
+    
     //범위 밖에 있는 경우
     if(index < start || index > end) return;
     //범위 안에 있으면 내려가며 다른 원소도 갱신
     tree[node] += dif;
-    if(start==end) return;
+    if (start ==end) return;
     int mid = (start+end) / 2;
     update(start, mid, node*2, index, dif);
     update(mid+1, end, node*2+1, index, dif);
-
-}
-
-int main(){
-
-    //구간 합 트리의 인덱스를 제외하고는 모두 인덱스 0부터 시작합니다.
-    //구간 합 트리 생성하기
-    init(0, a.size()-1, 1);
-    //구간 합 구하기
-    std::cout << "0부터 12까지의 구간 합: " << sum(0, a.size()-1, 1, 0, 12) << std::endl;
-    std::cout << "0부터 3까지의 구간 합: " << sum(0, a.size()-1, 1, 0, 3) << std::endl;
-
-
 }
