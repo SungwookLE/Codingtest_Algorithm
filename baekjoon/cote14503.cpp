c840366345c296b2ec0903de1b16ba1fab6f6481
SungwookLE <joker1251@naver.com>
2023-01-23 17:30:31 +0900

    Done: baekjoon #14503

--- baekjoon/cote14503.cpp
+++ baekjoon/cote14503.cpp
@@ -1,6 +1,7 @@
 /**
  * @file cote14503.cpp
  * @brief 백준 14053번 문제, 1차시도 테스트케이스 실패 음... 모니터링하는것도 만들어서 봤는데 잘못된거없는거 같은데...?
+ * @brief 2차시도 (2023-01-23) 성공, visited 체크하는 부분에서 섞여서 꼬여있었음..
  * @date 2023-01-17
  */
 
@@ -16,7 +17,7 @@ class solver_14503{
         N = _N;
         M = _M;
         room = vector<vector<int>>(N, vector<int>(M,0));
-        visited = vector<vector<int>>(N, vector<int>(M,0));
+        visited = vector<vector<int>>(N, vector<int>(M,-1));
 
         for(int row = 0 ; row < N ; ++row){
             for(int col =0 ; col < M ; ++col){
@@ -31,11 +32,11 @@ class solver_14503{
         int now_col = to[1];
         int now_Theta = to[2];
 
-        visited[now_row][now_col] = 1;
+        visited[now_row][now_col] = now_Theta;
         int next_row, next_col, next_Theta;
 
-        cout << now_row << " " << now_col << " " << now_Theta << endl;
-        monitoring(visited, to);
+        // cout << now_row << " " << now_col << " " << now_Theta << endl;
+        // monitoring(visited, to);
 
         if (count >3){
             
@@ -61,8 +62,7 @@ class solver_14503{
             }
 
             if(next_row >= 0 && next_col >= 0 && next_row < N && next_col < M){
-                if (room[next_row][next_col] == 0 && visited[next_row][next_col] == 0){
-                    cout << "후진" << endl;
+                if (room[next_row][next_col] == 0){
                     DFS({next_row, next_col, next_Theta}, 0);
                 }
             }
@@ -92,12 +92,10 @@ class solver_14503{
         }
 
         if(next_row >= 0 && next_col >= 0 && next_row < N && next_col < M){
-            if (room[next_row][next_col] == 0 && visited[next_row][next_col] == 0){
-                cout << "이동" << endl;
+            if (room[next_row][next_col] == 0 && visited[next_row][next_col] == -1){
                 DFS({next_row, next_col, next_Theta}, 0);
             }
-            else if (room[next_row][next_col] == 1 || visited[next_row][next_col] == 1){
-                cout << "회전" << endl;
+            else if (room[next_row][next_col] == 1 || visited[next_row][next_col] != -1){
                 DFS({now_row, now_col, next_Theta}, count+1);
             }
         }
@@ -108,29 +106,24 @@ class solver_14503{
     int answer(){
         for(auto row: visited){
             for (auto col: row){
-                ans += col;
+                if (col != -1)
+                    ans += 1;
             }
         }
         return ans;
     }
 
     void monitoring(vector<vector<int>> _visited, vector<int> now){
-
         vector<vector<char>> char_visited = vector<vector<char>>(N, vector<char>(M,' '));
 
         for(int r =0 ; r < _visited.size(); ++r){
             for(int c =0 ; c< _visited[0].size(); ++c){
-                char_visited[r][c] = max((char)(_visited[r][c] + '0'), (char)(room[r][c] + '0'));
-
-                if (now[2] == 0)
-                    char_visited[now[0]][now[1]] = '^';
-                else if (now[2] == 1)
-                    char_visited[now[0]][now[1]] = '>';
-                else if (now[2] == 2)
-                    char_visited[now[0]][now[1]] = 'v';
-                else if (now[2] == 3)
-                    char_visited[now[0]][now[1]] = '<';
-
+                if (room[r][c] == 1)
+                    char_visited[r][c] = '#';
+                else if (_visited[r][c] == -1)
+                    char_visited[r][c] = ' ';
+                else
+                    char_visited[r][c] = d[_visited[r][c]];
                 cout << char_visited[r][c] << " ";
             }
             cout << endl;
@@ -144,7 +137,8 @@ class solver_14503{
     vector<int> xyTheta= {0,0,0};
 
     private:
-    vector<int> d = {0, 1, 2, 3}; // 북, 동, 남, 서
+    vector<char> d = {'^', '>', 'v', '<'}; // 북, 동, 남, 서
+
     vector<vector<int>> delta = {{-1,0},{0,1}, {1,0}, {0,-1}}; // 북, 동, 남, 서
     int N, M;
     vector<vector<int>> room;
