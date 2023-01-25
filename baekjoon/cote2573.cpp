74a867bc3f51f05a35c84228343fb1c06f7d37e9
SungwookLE <joker1251@naver.com>
2023-01-25 22:54:53 +0900

    trying: .....#2573

--- baekjoon/cote2573.cpp
+++ baekjoon/cote2573.cpp
@@ -1,6 +1,7 @@
 /**
  * @brief 백준2573번 문제: 빙산
  * @brief 이런~ 빙산 1차시도: 시간초과 뭥미... ;;
+ * @brief 2차시도: 시간초과 실패 ㅠㅠ.. dfs를 포기해야하는건가 근데 bfs로 풀어도 이 문제같은 경우는 시간복잡도가 같은건데...
  * @date 2023-01-24
  * @file cote2573.cpp
  */
@@ -17,43 +18,56 @@ public:
     {
         N = _N;
         M = _M;
-        ices = vector<vector<int>>(N, vector<int>(M, 0));
+        ices_map = vector<vector<int>>(N, vector<int>(M, 0));
 
         for (int row = 0; row < N; ++row)
         {
             for (int col = 0; col < M; ++col)
             {
-                cin >> ices[row][col];
-                if (ices[row][col] != 0){
-                    ice_place.push_back({row, col});
-                }
+                cin >> ices_map[row][col];
             }
         }
     }
 
-    bool melt_ices()
+    void melt_ices()
     {
-        auto pictured_ices = ices;
-        bool isAllmelted = true;
-        vector<vector<int>> new_ice_place;
+        vector<vector<int>> copy_ices_map = ices_map;
 
-        for (auto ice : ice_place){
-            int row = ice[0];
-            int col = ice[1];
-            if (ices[row][col] != 0){
-                int count_zero = check_around(pictured_ices, row, col);
-                ices[row][col] -= count_zero;
-                if (ices[row][col] < 0)
-                    ices[row][col] = 0;
-
-                if (ices[row][col] > 0){
-                    isAllmelted = false;
-                    new_ice_place.push_back({row, col});
+        for (int row = 1; row < N - 1; ++row)
+        {
+            for (int col = 1; col < M - 1; ++col)
+            {
+                if (copy_ices_map[row][col] > 0)
+                {
+                    ices_map[row][col] -= check_around(copy_ices_map, row, col);
+                    ices_map[row][col] = max(0, ices_map[row][col]);
                 }
             }
         }
-        ice_place = new_ice_place;
-        return isAllmelted;
+
+        return;
+    }
+
+    int check_around(vector<vector<int>> copy_ices_map, int r, int c)
+    {
+        int next_r, next_c;
+        int count_zero = 0;
+
+        for (auto d : delta)
+        {
+            next_r = r + d.first;
+            next_c = c + d.second;
+
+            if (next_r >= 0 && next_c >= 0 && next_r < N && next_c < M)
+            {
+                if (copy_ices_map[next_r][next_c] == 0)
+                {
+                    count_zero += 1;
+                }
+            }
+        }
+
+        return count_zero;
     }
 
     void dfs(int r, int c)
@@ -67,7 +81,7 @@ public:
             next_c = c + d.second;
             if (next_r >= 1 && next_c >= 1 && next_r < N-1 && next_c < M-1)
             {
-                if (ices[next_r][next_c] != 0 && visited[next_r][next_c] == 0)
+                if (ices_map[next_r][next_c] > 0 && visited[next_r][next_c] == 0)
                 {
                     dfs(next_r, next_c);
                 }
@@ -77,32 +91,44 @@ public:
         return;
     }
 
-    int check_around(vector<vector<int>> pictured_ices, int r, int c)
+    int solve()
     {
-        int next_r, next_c;
-        int count_zero = 0;
+        int year = 0;
 
-        for (auto d : delta)
+        while (true)
         {
-            next_r = r + d.first;
-            next_c = c + d.second;
+            int count = 0;
+            visited = vector<vector<int>>(N, vector<int>(M, 0));
 
-            if (next_r >= 0 && next_c >= 0 && next_r < N && next_c < M)
+            for (int row = 1; row < N - 1; ++row)
             {
-                if (pictured_ices[next_r][next_c] == 0)
+                for (int col = 1; col < M - 1; ++col)
                 {
-                    count_zero += 1;
+                    if (ices_map[row][col] > 0 && visited[row][col] == 0)
+                    {
+                        if (count >= 2)
+                            return year;
+                        dfs(row, col);
+                        count += 1;
+                    }
                 }
             }
-        }
 
-        return count_zero;
+            if (count == 0)
+                return 0;
+
+            melt_ices();
+            year += 1;
+
+            // monitoring();
+        }
+        return 0;
     }
 
     void monitoring()
     {
         cout << endl;
-        for (auto r : ices)
+        for (auto r : ices_map)
         {
             for (auto c : r)
             {
@@ -112,40 +138,12 @@ public:
         }
     }
 
-    int solve()
-    {
-        bool isAllmelted = false;
-        int answer;
-
-        while (!isAllmelted)
-        {
-            int count = 0;
-            visited = vector<vector<int>>(N, vector<int>(M, 0));
-
-            isAllmelted = melt_ices();
-            answer += 1;
-
-            for (auto ice : ice_place){
-                int row = ice[0];
-                int col = ice[1];
-                if (ices[row][col] > 0 && visited[row][col] == 0){
-                    dfs(row, col);
-                    count += 1;
-                }
-            }
-            // monitoring();
-            if (count >= 2)
-                return answer;
-        }
-        return 0;
-    }
-
 private:
     int N, M;
-    vector<vector<int>> ices;
+    vector<vector<int>> ices_map;
+
     vector<pair<int, int>> delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     vector<vector<int>> visited;
-    vector<vector<int>> ice_place;
 };
 
 int main()
