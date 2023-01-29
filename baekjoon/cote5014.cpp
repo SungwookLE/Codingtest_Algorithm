38624212fd484b5244d3e40c957a75de0ecd62b2
SungwookLE <joker1251@naver.com>
2023-01-30 01:18:54 +0900

    Try;;; 5014

--- baekjoon/cote5014.cpp
+++ baekjoon/cote5014.cpp
@@ -2,6 +2,7 @@
  * @brief 백준 5014번 문제
  * @brief 근데 이 문제 DP 처럼도 풀 수 있나?
  * @brief 1차시도: 메모리 초과.. 아무래도  graph 만들어줄때 너무 SPARSE 하게 만들어서 배열 사이즈가 너무 커서 그런가보다...
+ * @brief 2차시도: 시간초과 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ 한 5시간 했나.. 하놔
  * @date 2023-01-27
  */
 
@@ -17,55 +18,49 @@ class solver_5014
 public:
     solver_5014(int _F, int _S, int _G, int _U, int _D) : F(_F), S(_S), G(_G), U(_U), D(_D)
     {
-        max_graph = F + 1 + 2 * U;
-        // graph = vector<vector<int>>(max_graph + 1, vector<int>(max_graph + 1, 0)); // 2차원배열이 아니고..., 1차원 배열로 바꾸셔야 겠소..,
-        // visited = vector<vector<int>>(max_graph + 1, vector<int>(max_graph + 1, 0));
+        max_graph = F;
+        graph = vector<vector<int>>(max_graph + 1); // make graph : graph[S-D] = {S-D+U, S-D-D} / graph[S] = {S+U, S-D} / graph[S+U] = {S+U+U, S+U-D} ...,
+        navigator = vector<vector<int>>(max_graph + 1);
         upAndDown = {U, -D};
+        vector<int> temp;
 
-        // make graph: graph[from][to] = cost
-        if (U > 0)
+        int ii = S;
+        temp.push_back(ii);
+
+        re1:
+        if (D > 0)
         {
-            for (int i = S + U; i <= max_graph; i = i + U)
+            for (int i = ii; i - D >= 1; i = i - D)
             {
-                // graph[i - U][i] = 1; 
-                graph.push_back({i-U, i});
-
-                if (D > 0)
-                {
-                    // graph[i][i-D] = 1;
-                    graph.push_back({i, i-D});
+                if (find(graph[i].begin(), graph[i].end(), i-D) == graph[i].end()){
+                    graph[i].push_back(i - D); // 아래 계단으로
+                    cout << i << ":(re1 down) " << i - D << endl;
+                    ii = i - D;
+                    temp.push_back(ii);
                 }
             }
         }
-        else
-        {
-            if (D > 0)
-            {
-                // graph[S][S-D] = 1;
-                graph.push_back({S, S-D});
-            }
-        }
-        sort(graph.begin(), graph.end(), [](auto a, auto b){
-            if (a[0] < b[0])
-                return true;
-            else if (a[0] ==b[0]){
-                if (a[1]<=b[1])
-                    return true;
-                else
-                    return false;
-            }
-            else
-                return false;
-        });
 
-        no_visited = graph;
+        if (U > 0)
+        {
+            for(auto a : temp){
+                for (int i = a; i + U <= max_graph; i = i + U)
+                {
+                    if (find(graph[i].begin(), graph[i].end(), i+U) == graph[i].end()){
+                        graph[i].push_back(i + U); // 윗 계단으로
+                        cout << i << ":(re2 up) " << i + U << endl;
+                        ii = i + U ;
+                        goto re1;
+                    }
+                }
+            }
+            temp.clear();
+        }
     }
 
     void solve()
     {
-        opens.push({S, S}); // opens = {from, to};
-        // visited[S][S] = 1;
-
+        opens.push({S, S, 0}); // opens = {from, to, diff};
         while (!opens.empty() && notFound)
         {
             vector<int> now = opens.front();
@@ -76,7 +71,15 @@ public:
             if (now_to == G)
             {
                 notFound = false;
-                cout << "FOUND\n";
+                int answer = 0;
+                while (now_to != S)
+                {
+                    // cout << "+++++: " << now_to << endl;
+                    now_to = navigator[now_to][0];
+                    answer += 1;
+                }
+
+                cout << answer << endl;
                 return;
             }
 
@@ -87,36 +90,38 @@ public:
 
                 if (next_from > 0 && next_from <= max_graph && next_to > 0 && next_to <= max_graph)
                 {
-                    vector<int> v = {next_from, next_to};
-                    auto i = find(no_visited.begin(), no_visited.end(), v);
-                    if ( i!= no_visited.end()){
-                        no_visited.erase(i);
-                        opens.push(v);
+                    auto i = find(graph[next_from].begin(), graph[next_from].end(), next_to);
+                    if (i != graph[next_from].end())
+                    {
+                        opens.push({next_from, next_to, now_to - now_from});
+                        navigator[next_to].push_back(next_from);
+                        graph[next_from].erase(i);
                     }
                 }
             }
-
-
         }
-        cout << "use the stairs\n";
+        cout << "use the stairs";
     }
 
     void monitor()
     {
+        int i = 0;
+
         for (auto r : graph)
         {
+            cout << "[" << i << "] ";
             for (auto c : r)
                 cout << c << " ";
             cout << endl;
+            i++;
         }
     }
 
 private:
     int F, S, G, U, D;
     vector<vector<int>> graph;
-    vector<vector<int>> no_visited;
-    vector<vector<int>> navigator;
     queue<vector<int>> opens;
+    vector<vector<int>> navigator;
     vector<int> upAndDown;
     bool notFound = true;
     int max_graph;
@@ -127,6 +132,7 @@ int main()
     int F, S, G, U, D;
     cin >> F >> S >> G >> U >> D;
     solver_5014 solver(F, S, G, U, D);
+    solver.monitor();
     solver.solve();
 
     return 0;
